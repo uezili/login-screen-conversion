f4bd38d5c4ee0ff136929517e5ac53a6d93bbe7b
Wesley Maciel <weslmaciel@gmail.com>
2022-12-18 10:12:03 -0300

    Import components.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import './index.css';
+import { Button } from './Components/Button';
+import { UserBox } from './Components/Userbox';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className='login-box'>
+      <h2>LOGIN</h2>
+      <form>
+        <UserBox type="text" label="User" />
+        <UserBox type="password" label="Password" />
+        <Button content="ENTER" />
+      </form>
+    </div>
   </React.StrictMode>
 );
